0abbe5ffa29e9417d749eae31745b71d88e35cf8
Tolga Karasay <tolgakarasay@gmail.com>
2021-11-16 03:59:57 +0300

    Console Programlama

---
+++ console_programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Adınız: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınız: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname + "!");
+        }
+    }
+}
